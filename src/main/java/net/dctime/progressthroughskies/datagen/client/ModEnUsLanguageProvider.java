0bba4f5aadb8eaaa077a03617e4ac765faedeb9e
dctime <daniel.chu916@gmail.com>
2023-06-24 15:05:08 +0800

    Feature: Editing documents

--- src/main/java/net/dctime/progressthroughskies/datagen/client/ModEnUsLanguageProvider.java
+++ src/main/java/net/dctime/progressthroughskies/datagen/client/ModEnUsLanguageProvider.java
@@ -1,6 +1,7 @@
 package net.dctime.progressthroughskies.datagen.client;
 
 import net.dctime.progressthroughskies.events.mod.ProgressThroughSkies;
+import net.dctime.progressthroughskies.registers.ModBlocks;
 import net.dctime.progressthroughskies.registers.ModItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.LanguageProvider;
@@ -13,7 +14,9 @@ public class ModEnUsLanguageProvider extends LanguageProvider
     }
 
     @Override
-    protected void addTranslations() {
+    protected void addTranslations()
+    {
+        this.addItem(ModItems.DUSTED_BEDROCK, "Dusted Bedrock");
         this.addItem(ModItems.BEDROCK_DUST, "Bedrock Dust");
     }
 }
