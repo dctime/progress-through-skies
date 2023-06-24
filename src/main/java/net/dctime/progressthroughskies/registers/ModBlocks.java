d03d43918d83f17f8bb33829ef856692d507aabc
dctime <daniel.chu916@gmail.com>
2023-06-24 14:52:32 +0800

    Feature: Pointing dusted_bedrock and hold right click to get bedrock_dust.

--- src/main/java/net/dctime/progressthroughskies/registers/ModBlocks.java
+++ src/main/java/net/dctime/progressthroughskies/registers/ModBlocks.java
@@ -1,7 +1,9 @@
 package net.dctime.progressthroughskies.registers;
 
 import net.dctime.progressthroughskies.events.mod.ProgressThroughSkies;
+import net.dctime.progressthroughskies.registers.blocks.DustedBedrockBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -13,7 +15,7 @@ public class ModBlocks
 {
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ProgressThroughSkies.MODID);
 
-    public static final RegistryObject<Block> DUSTED_BEDROCK = BLOCKS.register("dusted_bedrock", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
+    public static final RegistryObject<Block> DUSTED_BEDROCK = BLOCKS.register("dusted_bedrock", () -> new DustedBedrockBlock(BlockBehaviour.Properties.copy(Blocks.BEDROCK)));
 
     public static void register(IEventBus eventBus)
     {
--- src/main/java/net/dctime/progressthroughskies/registers/blocks/DustedBedrockBlock.java
+++ src/main/java/net/dctime/progressthroughskies/registers/blocks/DustedBedrockBlock.java
@@ -1,11 +1,39 @@
 package net.dctime.progressthroughskies.registers.blocks;
 
+import com.mojang.logging.LogUtils;
+import net.dctime.progressthroughskies.registers.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+import java.util.Random;
 
 public class DustedBedrockBlock extends Block
 {
+    private static final Logger LOGGER = LogUtils.getLogger();
     public DustedBedrockBlock(Properties pProperties)
     {
         super(pProperties);
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        LOGGER.debug("OUCH");
+        ItemStack spawnItemStack = new ItemStack(ModItems.BEDROCK_DUST.get());
+        Random random = new Random();
+        if (random.nextInt(2) == 1 && pPlayer.isShiftKeyDown())
+        {
+            pLevel.addFreshEntity(new ItemEntity(pLevel, pPos.getX()+0.5, pPos.getY()+1, pPos.getZ()+0.5, spawnItemStack));
+        }
+        return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
     }
 }
