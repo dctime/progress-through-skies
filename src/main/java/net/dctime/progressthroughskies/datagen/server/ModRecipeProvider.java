9b56f9366d30a796deb32721c07ebf1a5bfeed65
dctime <daniel.chu916@gmail.com>
2023-06-26 11:17:33 +0800

    Feature: Add gravel dust and sand dust recipe

--- src/main/java/net/dctime/progressthroughskies/datagen/server/ModRecipeProvider.java
+++ src/main/java/net/dctime/progressthroughskies/datagen/server/ModRecipeProvider.java
@@ -5,6 +5,7 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 
 import java.util.function.Consumer;
@@ -20,5 +21,13 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
         ShapedRecipeBuilder.shaped(ModItems.BEDROCK_GRAVEL.get(), 1)
                 .pattern("XX").pattern("XX").define('X', ModItems.BEDROCK_DUST.get())
                 .unlockedBy("has_bedrock_dust", has(ModItems.BEDROCK_DUST.get())).save(consumer);
+
+        ShapedRecipeBuilder.shaped(Blocks.SAND, 1)
+                .pattern("XX").pattern("XX").define('X', ModItems.SAND_DUST.get())
+                .unlockedBy("has_sand_dust", has(ModItems.SAND_DUST.get())).save(consumer);
+
+        ShapedRecipeBuilder.shaped(Blocks.GRAVEL, 1)
+                .pattern("XX").pattern("XX").define('X', ModItems.GRAVEL_DUST.get())
+                .unlockedBy("has_gravel_dust", has(ModItems.GRAVEL_DUST.get())).save(consumer);
     }
 }
