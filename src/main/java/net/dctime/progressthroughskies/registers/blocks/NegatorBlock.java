b66c8239fba2fd12a19f306ba212c27d0ad742cc
dctime <daniel.chu916@gmail.com>
2023-08-11 21:42:13 +0800

    Feature: Structure Check Done

--- src/main/java/net/dctime/progressthroughskies/registers/blocks/NegatorBlock.java
+++ src/main/java/net/dctime/progressthroughskies/registers/blocks/NegatorBlock.java
@@ -11,6 +11,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -40,9 +41,42 @@ public class NegatorBlock extends BaseEntityBlock
 
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit)
     {
+        boolean isValid =
+                // First Layer
+                isBlockRightThere(pLevel, pPos, Blocks.GRAY_WOOL, 0, -2, 0) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.GRAY_WOOL, 1, -2, 0) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.GRAY_WOOL, 0, -2, 1) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.GRAY_WOOL, -1, -2, 0) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.GRAY_WOOL, 0, -2, -1) &&
+                        // Second Layer
+                        isBlockRightThere(pLevel, pPos, Blocks.GRAY_WOOL, 2, -1, 0) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.GRAY_WOOL, -2, -1, 0) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.GRAY_WOOL, 0, -1, 2) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.GRAY_WOOL, 0, -1, -2) &&
+                        //Third Layer
+                        isBlockRightThere(pLevel, pPos, Blocks.GRAY_WOOL, 2, 0, 0) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.GRAY_WOOL, -2, 0, 0) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.GRAY_WOOL, 0, 0, 2) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.GRAY_WOOL, 0, 0, -2) &&
+
+                        isBlockRightThere(pLevel, pPos, Blocks.BLUE_WOOL, 3, 0, 3) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.BLUE_WOOL, 2, 0, 3) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.BLUE_WOOL, 3, 0, 2) &&
+
+                        isBlockRightThere(pLevel, pPos, Blocks.BLUE_WOOL, 3, 0, -3) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.BLUE_WOOL, 2, 0, -3) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.BLUE_WOOL, 3, 0, -2) &&
+
+                        isBlockRightThere(pLevel, pPos, Blocks.BLUE_WOOL, -3, 0, 3) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.BLUE_WOOL, -2, 0, 3) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.BLUE_WOOL, -3, 0, 2) &&
+
+                        isBlockRightThere(pLevel, pPos, Blocks.BLUE_WOOL, -3, 0, -3) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.BLUE_WOOL, -2, 0, -3) &&
+                        isBlockRightThere(pLevel, pPos, Blocks.BLUE_WOOL    , -3, 0, -2);
 
 
-        if (!pLevel.isClientSide())
+        if (!pLevel.isClientSide() && isValid)
         {
             BlockEntity entity = pLevel.getBlockEntity(pPos);
             if (entity instanceof NegatorBlockEntity)
@@ -56,5 +90,15 @@ public class NegatorBlock extends BaseEntityBlock
             }
         }
         return InteractionResult.SUCCESS;
+    }
+
+    private boolean isBlockRightThere(Level level, BlockPos pos, Block block, int xOffset, int yOffset, int zOffset)
+    {
+        if (!level.isClientSide())
+        {
+            return level.getBlockState(new BlockPos(pos.getX()+xOffset, pos.getY()+yOffset, pos.getZ()+zOffset)).getBlock() == block;
+        }
+
+        return false;
     }
 }
